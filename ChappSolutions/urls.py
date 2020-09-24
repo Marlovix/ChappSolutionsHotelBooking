f3c955ac866c46eee2c4122a37ae0b21cff04a91
Marlovix <Marlovix@users.noreply.github.com>
2020-09-24 21:41:44 +0100

    Test

--- ChappSolutions/urls.py
+++ ChappSolutions/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('hotelBooking.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
