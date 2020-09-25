2b32801f99c13ce496857f3863162b4796bbe0f1
Marlovix <Marlovix@users.noreply.github.com>
2020-09-25 01:21:40 +0100

    Test

--- ChappSolutions/settings/base.py
+++ ChappSolutions/settings/base.py
@@ -73,8 +73,8 @@ WSGI_APPLICATION = 'ChappSolutions.wsgi.application'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # Password validation
